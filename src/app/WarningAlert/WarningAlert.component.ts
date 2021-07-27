d68ab5ab407a6f5821bf3ed9c9b1b8e3bcd49353
krishnendu paul <krishnendupaul.mca@gmail.com>
2021-07-27 15:32:31 +0530

    ok

---
+++ src/app/WarningAlert/WarningAlert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-warring',
+    templateUrl:'./warningalert.component.html',
+    styleUrls:['./WarningAlert.component.css']
+})
+
+export class WarningAlertComponent{    
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import {WarningAlertComponent} from './WarningAlert/WarningAlert.component'
+@NgModule({
+  declarations: [
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
